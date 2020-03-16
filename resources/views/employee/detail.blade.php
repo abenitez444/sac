1b1b0791d8289b363f899134998d2824e324d4f4
Alejandro Benitez <alejandro.brice.614@gmail.com>
2020-03-16 14:17:12 -0400

    [FIX] Diseño de vista detail employee lista

--- resources/views/employee/detail.blade.php
+++ resources/views/employee/detail.blade.php
@@ -14,41 +14,36 @@
           <div class="sidebar-brand-text mx-3 push ml-3">
             <i class="fas fa-atlas text-primary sidebar-brand-icon rotate-n-15"></i> <b class="text-primary"> SIGESPRO</b>
           </div>
-          <div class="row">
+       
               <!--Grid column-->
-              <div class="col-lg-12 col-md-12 mb-lg-0 mb-4">
-                  <!--Background color-->
-                  <div class="card-up info-color"></div>
-                  <!--Avatar-->
-                  <div class="avatar mx-auto white">
-                    <img src="{{asset('/img/avatar.png')}}" class="rounded-circle img-fluid">
-                  </div>
-                  <div class="card-body">
-                    <!--Name-->
-                    <div class="row justify-content-center">
-                      <div class="col-md-12">
-                         <h4 class="font-weight-bold text-center mb-4 mt-3"><span id="name"></span></h4>
-                          <span id="nac"></span>-
-                          <span id="ci"></span> / <span id="tlf"></span>
-                      </div>
-                    </div>
-                    <hr>
-                    <!--Quotation-->
-                    <p class="dark-grey-text mt-4"><i class="fa fa-quote-left pr-2"></i><span id="mail"></span>.</p>
-                  </div>
-              </div>
-          </div>
+        <div class="col-lg-12 col-md-12 mb-lg-0 mb-5">
+          <!--Background color-->
+            <div class="card-up info-color"></div>
+          <!--Avatar-->
+            <div class="avatar mx-auto white">
+              <img src="{{asset('/img/avatar.png')}}" class="rounded-circle img-fluid" style=" border: 2px solid #aaa;">
             </div>
-          </div>
-          <div class="row justify-content-center">
-            <div class="col-md-12">
-              <span id="name" class="font-weight-bold" style=""></span>
+            <div class="card-body">
+              <!--Name-->
+                <div class="col-md-12 mt-3">
+                   <h4 class="font-weight-bold text-center mb-4"><span id="name"></span></h4>
+                   <i class="fas fa-id-card text-primary fa-lg" title="Cédula de identidad."></i> <span id="nac" class="font-weight-bold mt-3"></span>-
+                    <span id="ci" class="font-weight-bold mt-3"></span> 
+                </div>
+              <hr>
+                <div class="col-md-12">
+                    <i class="fas fa-phone-square text-primary fa-lg" title="Teléfono"></i> <span id="tlf" class="font-weight-bold"></span>
+                </div>
+              <hr>
+              <!--Quotation-->
+              <div class="col-md-12">
+                <p class="dark-grey-text "><i class="fas fa-envelope text-primary fa-lg" title="Correo electrónico"></i> <span id="mail" class="font-weight-bold"></span>.</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-{{--  <div class="modal-footer">
-  </div>--}}
 </div>
+
