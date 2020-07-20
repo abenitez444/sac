75755fdc4e523769391f51c194afd23a0a0416aa
abenitez444 <alejandro.brice.614@gmail.com>
2020-07-20 19:01:07 -0400

    [FIX] - View Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,15 @@
 @section('card-title', 'Inicio')
 @section('card-subtitle', 'Pagina de inicio')
 @section('content')
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, ut cumque quaerat, iusto dolore ipsam eligendi dolorum ipsa incidunt consequuntur minus consectetur quidem atque dolores. Consequatur laboriosam mollitia, accusantium placeat.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, ut cumque quaerat, iusto dolore ipsam eligendi dolorum ipsa incidunt consequuntur minus consectetur quidem atque dolores. Consequatur laboriosam mollitia, accusantium placeat.</p>
+    <!-- Sidebar - Brand -->
+<div class="row mt-5">
+	<div class="col-sm-8 col-md-12 col-lg-12 mt-5 form-group">	
+	  <a class="sidebar-brand d-flex align-items-center justify-content-center text-dark fa-lg" href="{{ route('home') }}">
+	    <div class="sidebar-brand-icon rotate-n-15 ">
+	      <i class="fas fa-atlas"></i>
+	    </div>
+	    <div class="sidebar-brand-text mx-3 text-dark font-weight-bold">Gestión de condóminio y (GCA)</div>
+	  </a>
+	</div>
+</div>
 @endsection
